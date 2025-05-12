9871199d9c0bc4ee29355d63730850e25aced181
symihad <symihad@gmail.com>
2025-05-12 16:45:35 +0600

    Initial Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Slider;
+use Image;
+use Illuminate\Support\Carbon;
+
+class HomeController extends Controller
+{
+
+
+    public function homeslider () {
+        $sliders = Slider::latest()->get();
+        return view('admin.slider.index', compact('sliders'));
+    }
+
+
+    public function addslider() {
+        return view('admin.slider.create');
+    }
+
+    public function storeslider(Request $request) {
+        // $this->validate($request, [
+
+        //     'image'=>'required|mimes:jpg,jpeg,png',
+        // ]);
+
+
+
+         $slider = $request->file('image');
+        //  $name_gen = hexdec(uniqid()).'.'.$slider->getClientOriginalExtension();
+         $name_gen = hexdec(uniqid()).'.'.time().'.'.$slider->getClientOriginalExtension();
+         Image::make($slider)->resize(1920,1088)->save('image/slider/'.$name_gen);
+         $last_image = 'image/slider/'.$name_gen;
+        //  $last_image = 'image/slider/'.$name_gen;
+         Slider::insert([
+            'title' => $request->title,
+            'dec'=> $request->dec,
+            'image' =>$last_image,
+            'created_at' =>Carbon::now()
+        ]);
+        return redirect()->route('home.slider')->with('message', 'barnd successfully');
+    }
+
+
+    public function deleteslider($id) {
+        $imagedelet = Slider::find($id);
+        $old_image = $imagedelet->image;
+        unlink($old_image);
+
+        Slider::find($id)->delete();
+        return redirect()->route('home.slider')->with('message', 'slider delete successfully');
+    }
+
+
+    public function editlider($id) {
+        $sliders = Slider::find($id);
+        //  dd($sliders);
+        return view('admin.slider.edit', compact('sliders'));
+    }
+
+    public function updateslider(Request $request , $id) {
+        $image = $request->file('image');
+        if($image) {
+            $old_image = $request->old_image;
+            $slider_image = $request->file('slider_image');
+            $name_gen = hexdec(uniqid()).'.'.$slider_image->getClientOriginalExtension();
+                Image::make($slider_image)->resize(1920,1088)->save('image/slider/'.$name_gen);
+                $last_image = 'image/slider/'.$name_gen;
+
+                unlink($old_image);
+
+                Slider::find($id)->update([
+                    'title' => $request->title,
+                    'dec'=> $request->dec,
+                    'image' =>$last_image
+                ]);
+                return redirect()->route('home.slider')->with('message', 'barnd updated successfully');
+        }else {
+            Slider::find($id)->update([
+                'title' => $request->title,
+                'dec'=> $request->dec,
+
+            ]);
+            return redirect()->route('home.slider')->with('message', 'barnd updated successfully');
+        }
+
+    }
+}
---
+++ resources/views/layouts/body/header.blade.php
@@ -0,0 +1,65 @@
+
+@php
+  $social = DB::table('sociallinks')->first();
+
+@endphp
+
+ <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href={{ route('main.home') }}><span>No</span>ngor</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="index.html" class="logo mr-auto">
+        {{-- <img src="backend/assets/img/NONGOR-LOGO-ENG.png" alt="" style="height: 100px; width: 500px" class="img-fluid"> --}}
+      </a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href={{ route('main.home') }}>Home</a></li>
+
+          {{-- <li class="drop-down"><a href="">About</a>
+            <ul>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="team.html">Team</a></li>
+              <li><a href="testimonials.html">Testimonials</a></li>
+              <li class="drop-down"><a href="#">Deep Drop Down</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li> --}}
+
+          <li class="drop-down"><a href="">Services</a>
+            <ul>
+              <li><a href="about.html">Agro</a></li>
+              <li><a href="team.html">Real Eastate</a></li>
+              <li><a href="testimonials.html">Shop</a></li>
+              <li><a href="testimonials.html">Restaurant</a></li>
+            </ul>
+          </li>
+
+          {{-- <li><a href="services.html">Services</a></li> --}}
+          {{-- <li><a href="{{route('portfolio')}}">Portfolio</a></li> --}}
+          {{-- <li><a href="pricing.html">Pricing</a></li> --}}
+          {{-- <li><a href="blog.html">Blog</a></li> --}}
+          <li><a href="{{route('contact')}}">About Us</a></li>
+
+          <li><a href="{{route('contact')}}">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <div class="header-social-links">
+        <a href="{{$social->tw}}" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="{{$social->fb}}" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="{{$social->ins}}" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="{{$social->ln}}" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header>
